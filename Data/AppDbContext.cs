dd830708f33f67489c785d85e1754d1cc62f58ad
SergioHMagalhaes <ssergioh3@gmail.com>
2025-05-07 13:09:07 -0300

    feat: adiciona migrations do banco

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BookstoreManagementApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookstoreManagementApi.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<AuthorModel> Authors { get; set; }
+    public DbSet<BookModel> Books { get; set; }
+    public DbSet<GenderModel> Gender { get; set; }
+}
---
+++ Models/GenderModel.cs
@@ -0,0 +1,7 @@
+﻿namespace BookstoreManagementApi.Models;
+
+public class GenderModel
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
